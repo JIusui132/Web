45c66b4d04960c3c06c5452d5267032601822978
JIusui132 <160123418+JIusui132@users.noreply.github.com>
2024-06-04 12:52:09 +0300

    Update code.js

--- WEB_JS1/code.js
+++ WEB_JS1/code.js
@@ -1,195 +1,188 @@
+console.log("ІНСТРУКЦІЯ");
+console.log("Для використання функції triangle введіть наступну команду:");
+console.log("triangle(значення_аргумента_1, 'тип_аргумента_1', значення_аргумента_2, 'тип_аргумента_2');");
+console.log("Перелік можливих типів аргументів:");
+console.log("- leg (катет)");
+console.log("- hypotenuse (гіпотенуза)");
+console.log("- adjacent angle (прилеглий до катета кут)");
+console.log("- opposite angle (протилежний до катета кут)");
+console.log("- angle (один з двох гострих кутів)");
+console.log("Приклад виклику функцій:");
+console.log("triangle(4, 'leg', 8, 'hypotenuse');");
+console.log("___________________________________________________________________");
+
 function triangle(val1, type1, val2, type2) {
-    const validTypes = ["leg", "hypotenuse", "adjacent", "opposite", "angle"];
+    // Перевірка на наявність правильних типів
+    const validTypes = ["leg", "hypotenuse", "adjacent angle", "opposite angle", "angle"];
     if (!validTypes.includes(type1) || !validTypes.includes(type2)) {
         console.log("Будь ласка, перечитайте інструкцію та введіть правильні типи.");
         return "failed";
     }
 
-    if (val1 <= 0 || val2 <= 0) {
-        console.log("Значення мають бути більше 0.");
+    // Перевірка на коректність введених значень
+    if ((typeof val1 !== 'number' && isNaN(parseFloat(val1))) || (typeof val2 !== 'number' && isNaN(parseFloat(val2)))) {
+        console.log("Введені значення повинні бути числами або числовими рядками.");
+        return "failed";
+    }
+
+    // Перевірка на від'ємні значення
+    if (val1 < 0 || val2 < 0) {
+        console.log("Введені значення не можуть бути від'ємними числами.");
         return "failed";
     }
 
     let c, a, b, alpha, beta;
+
+    // Перевірка усіх можливих порядків типів аргументів
     switch (type1) {
-        case "leg":
-            a = val1;
-            switch (type2) {
-                case "leg":
-                    b = val2;
-                    c = Math.sqrt(a ** 2 + b ** 2);
-                    alpha = Math.atan(b / a) * 180 / Math.PI;
-                    beta = 90 - alpha;
-                    break;
+        case "adjacent angle":
+            switch(type2){
                 case "hypotenuse":
+                    alpha = val1;
+                    beta = 90 - alpha;
+                    a = Math.sqrt(c * c - b * b); 
                     c = val2;
                     if (a >= c) {
-                        console.log("Катет не може бути більший або рівний гіпотенузі.");
+                        console.log("Значення катету не може бути більшим або рівним за значення гіпотенузи.");
                         return "failed";
                     }
-                    b = Math.sqrt(c ** 2 - a ** 2);
-                    alpha = Math.acos(a / c) * 180 / Math.PI;
+                    b = (c * Math.sin(alpha * (Math.PI / 180)));
+                    break;
+
+                    case "leg":
+                        beta = val1;
+                        a = val2;
+                        b = (a * Math.tan(beta * (Math.PI / 180)));
+                        c = Math.sqrt(b * b + a * a);
+                        alpha = 90 - beta;
+                        break;
+                }
+                break;
+            
+
+
+
+
+
+                case "opposite angle":
+                    switch(type2) {
+                        case "leg":
+                            alpha = val1;
+                            a = val2;
+                            c = (a / Math.sin(alpha * (Math.PI / 180)));
+                            b = Math.sqrt(c * c - a * a);
+                            beta = 90 - alpha;
+                            break;
+
+                            case "hypotenuse":
+                                alpha = val1;
+                                beta = 90 - alpha;
+                                c = val2;
+                                a = (c * Math.sin(alpha * (Math.PI / 180)));
+                                b = Math.sqrt(c * c - a * a);
+                                break;
+                            }
+                        break;
+
+
+
+
+        case "leg":
+            switch (type2) {
+                case "hypotenuse":
+                    a = val1;
+                    c = val2;
+                    if (a >= c) {
+                        console.log("Значення катету не може бути більшим або рівним за значення гіпотенузи.");
+                        return "failed";
+                    }
+                    b = Math.sqrt(c * c - a * a);
+                    alpha = Math.asin(a / c) * (180 / Math.PI);
                     beta = 90 - alpha;
                     break;
-                case "adjacent":
-                    beta = val2;
-                    if (beta >= 90 || beta <= 0) return "failed";
-                    b = a / Math.tan(beta * Math.PI / 180);
-                    c = Math.sqrt(a ** 2 + b ** 2);
-                    alpha = 90 - beta;
-                    break;
-                case "opposite":
-                    alpha = val2;
-                    if (alpha >= 90 || alpha <= 0) return "failed";
-                    b = a * Math.tan(alpha * Math.PI / 180);
-                    c = Math.sqrt(a ** 2 + b ** 2);
+                case "leg":
+                    a = val1;
+                    b = val2;
+                    c = Math.sqrt(b * b + a * a);
+                    alpha = Math.asin(a / c) * (180 / Math.PI);
                     beta = 90 - alpha;
                     break;
                 case "angle":
-                    return "failed";
+                    a = val1;
+                    alpha = val2;
+                    beta = 90 - alpha;
+                    b = a / Math.tan(alpha * (Math.PI / 180));
+                    c = Math.sqrt(a * a + b * b);
+                    break;
+                case "adjacent angle":
+                    a = val1;
+                    beta = val2;
+                    alpha = 90 - beta;
+                    c =  a / Math.cos(beta * (Math.PI / 180));
+                    b = Math.sqrt(c * c - a * a);
+                    break;
+                case "opposite angle":
+                    a = val1;
+                    alpha = val2;
+                    beta = 90 - alpha;
+                    b = a / Math.tan(alpha * (Math.PI / 180));
+                    c = a / Math.sin(alpha * (Math.PI / 180));
+                    break;
             }
             break;
         case "hypotenuse":
-            c = val1;
             switch (type2) {
                 case "leg":
+                    c = val1;
                     a = val2;
-                    if (a >= c) {
-                        console.log("Катет не може бути більший або рівний гіпотенузі.");
+                    if (c <= a) {
+                        console.log("Значення гіпотенузи не може бути меншим або рівним за значення катету.");
                         return "failed";
                     }
-                    b = Math.sqrt(c ** 2 - a ** 2);
-                    alpha = Math.asin(a / c) * 180 / Math.PI;
+                    b = Math.sqrt(c * c - a * a);
+                    alpha = Math.asin(a / c) * (180 / Math.PI);
                     beta = 90 - alpha;
                     break;
-                case "hypotenuse":
-                    return "failed";
-                case "adjacent":
+                case "angle":
+                    c = val1;
                     beta = val2;
-                    if (beta >= 90 || beta <= 0) return "failed";
-                    a = c * Math.cos(beta * Math.PI / 180);
-                    b = Math.sqrt(c ** 2 - a ** 2);
                     alpha = 90 - beta;
+                    a = (c * Math.sin(beta * (Math.PI / 180))) / Math.sin(90 * (Math.PI / 180));
+                    b = Math.sqrt(c * c - a * a);
                     break;
-                case "opposite":
-                    alpha = val2;
-                    if (alpha >= 90 || alpha <= 0) return "failed";
-                    b = c * Math.sin(alpha * Math.PI / 180);
-                    a = Math.sqrt(c ** 2 - b ** 2);
-                    beta = 90 - alpha;
-                    break;
-                case "angle":
-                    return "failed";
-            }
-            break;
-        case "adjacent":
-            beta = val1;
-            if (beta >= 90 || beta <= 0) {
-                console.log("Прилеглий кут має бути менше за 90 градусів та більше за 0.");
-                return "failed";
-            }
-            switch (type2) {
-                case "leg":
-                    a = val2;
-                    if (a <= 0) {
-                        console.log("Катет має бути більше 0.");
-                        return "failed";
-                    }
-                    b = a * Math.tan(beta * Math.PI / 180);
-                    c = a / Math.cos(beta * Math.PI / 180);
-                    alpha = 90 - beta;
-                    break;
-                case "hypotenuse":
-                    c = val2;
-                    if (c <= 0) {
-                        console.log("Гіпотенуза має бути більше 0.");
-                        return "failed";
-                    }
-                    a = c * Math.cos(beta * Math.PI / 180);
-                    b = Math.sqrt(c ** 2 - a ** 2);
-                    alpha = 90 - beta;
-                    break;
-                case "adjacent":
-                    return "failed";
-                case "opposite":
-                    return "failed";
-                case "angle":
-                    return "failed";
-            }
-            break;
-        case "opposite":
-            alpha = val1;
-            if (alpha >= 90 || alpha <= 0) {
-                console.log("Протилежний кут має бути менше за 90 градусів та більше за 0.");
-                return "failed";
-            }
-            switch (type2) {
-                case "leg":
-                    a = val2;
-                    if (a <= 0) {
-                        console.log("Катет має бути більше 0.");
-                        return "failed";
-                    }
-                    b = a * Math.tan(alpha * Math.PI / 180);
-                    c = a / Math.sin(alpha * Math.PI / 180);
-                    beta = 90 - alpha;
-                    break;
-                case "hypotenuse":
-                    c = val2;
-                    if (c <= 0) {
-                        console.log("Гіпотенуза має бути більше 0.");
-                        return "failed";
-                    }
-                    b = c * Math.sin(alpha * Math.PI / 180);
-                    a = Math.sqrt(c ** 2 - b ** 2);
-                    beta = 90 - alpha;
-                    break;
-                case "adjacent":
-                    return "failed";
-                case "opposite":
-                    return "failed";
-                case "angle":
-                    return "failed";
             }
             break;
         case "angle":
             switch (type2) {
                 case "leg":
-                    return "failed";
-                case "hypotenuse":
-                    return "failed";
-                case "adjacent":
-                    beta = val1;
-                    if (beta >= 90 || beta <= 0) return "failed";
-                    a = val2;
-                    if (a <= 0) {
-                        console.log("Катет має бути більше 0.");
-                        return "failed";
-                    }
-                    b = a * Math.tan(beta * Math.PI / 180);
-                    c = a / Math.cos(beta * Math.PI / 180);
-                    alpha = 90 - beta;
-                    break;
-                case "opposite":
                     alpha = val1;
-                    if (alpha >= 90 || alpha <= 0) return "failed";
                     a = val2;
-                    if (a <= 0) {
-                        console.log("Катет має бути більше 0.");
-                        return "failed";
-                    }
-                    b = a * Math.tan(alpha * Math.PI / 180);
-                    c = a / Math.sin(alpha * Math.PI / 180);
                     beta = 90 - alpha;
+                    c = a / (Math.sin(alpha * (Math.PI / 180)) / Math.sin(90 * (Math.PI / 180)));
+                    b = Math.sqrt(c * c - a * a);
                     break;
-                case "angle":
-                    return "failed";
+                case "hypotenuse":
+                    alpha = val1;
+                    c = val2;
+                    beta = 90 - alpha;
+                    a = (c * Math.sin(alpha * (Math.PI / 180))) / Math.sin(90 * (Math.PI / 180));
+                    b = Math.sqrt(c * c - a * a);
+                    break;
             }
             break;
     }
 
-    console.log(`c = ${c}, a = ${a}, b = ${b}, alpha = ${alpha}, beta = ${beta}`);
+    // Перевірка на негострі кути
+    if (alpha >= 90 || beta >= 90) {
+        console.log("Введені кути не є гострими кутами.");
+        return "failed";
+    }
+
+    console.log(`a = ${a}`);
+    console.log(`b = ${b}`);
+    console.log(`c = ${c}`);
+    console.log(`alpha = ${alpha}`);
+    console.log(`beta = ${beta}`);
+
     return "success";
 }
-
-triangle(5, "leg", 3, "hypotenuse"); 
